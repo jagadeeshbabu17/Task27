3dc105c084c6e8963e05472c9679384b45f54ea2
unknown <katurijagadeesshabu27@gmail.com>
2021-01-20 17:29:18 +0530

    new

--- src/org/sample/Firefox.java
+++ src/org/sample/Firefox.java
@@ -8,5 +8,17 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.gecko.driver", "C:\\Users\\jb\\selinium\\Sel2\\Driver\\geckodriver.exe");
 	WebDriver dev=new FirefoxDriver();
 	dev.get("http://www.facebook.com");
+	
+	System.out.println("12345");
+	System.out.println("kkkk");System.out.println("12345");
+	System.out.println("kkkk");System.out.println("12345");
+	System.out.println("kkkk");System.out.println("12345");
+	System.out.println("kkkk");System.out.println("12345");
+	System.out.println("kkkk");System.out.println("12345");
+	System.out.println("kkkk");
+	
+	
+	
+	
 }
 }
